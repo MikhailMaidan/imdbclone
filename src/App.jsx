adaab7eda6567c32ee2a275917f7ac7c991679b7
Nikolai Maidan <axroit.08@gmail.com>
2024-05-26 00:34:35 +0200

    fix: some files and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import Header from '@/components/Header/Header.jsx'
 import BDscreen from '@/components/BDscreen/BDscreen.jsx'
-import BlackBackGround from '@/components/BlackBackGround/BlackBackGround.jsx'
-import LiveShow from '@/components/LiveShow/LiveShow.jsx'
-import MostPopular from '@/components/MostPopular/MostPopular.jsx'
-import MoviesForYou from '@/components/MoviesForYou/MoviesForYou.jsx'
-import Bangla from '@/components/LatestBanglaMusic/Latest.jsx'
-import Footer from '@/components/Footer/footer.jsx'
 import MovieImageContainer from '@/components/MovieImageContainer/MovieImageContainer.jsx'
 import NewLiveShow from './components/NewLiveShow/NewLiveShow.jsx'
-import NewMostPopular from './components/NewMostPopular/NewMostPopular.jsx'
-import NewMoviesForYou from './components/NewMoviesForYou/NewMoviesForYou.jsx'
+// import NewMostPopular from './components/NewMostPopular/NewMostPopular.jsx'
+// import NewMoviesForYou from './components/NewMoviesForYou/NewMoviesForYou.jsx'
 import './App.scss'
 
 export default function App() {
--- src/components/NewLiveShow/NewLiveShow.jsx
+++ src/components/NewLiveShow/NewLiveShow.jsx
@@ -1,65 +1,53 @@
-import React from 'react';
+import { useState, useLayoutEffect } from "react";
 import './NewLiveShow.scss'
-import Arrow from '@/assets/images/Live_Show/arrow.png'
-import TheShow from "@/assets/images/Live_Show/The_show.png"
-import Kpop from "@/assets/images/Live_Show/K_pop.png"
-import Bangladesh from "@/assets/images/Live_Show/banglagesh.png"
-import Kapil from "@/assets/images/Live_Show/kapil.png"
-import Minions from "@/assets/images/Live_Show/minions.png"
-import {BaseFetcher} from "@/composables/fetcher/BaseFetcher.js"
-
-let LiveShowImages;
-
-async function getMovies() {
-
-    const baseFetcher = new BaseFetcher("/api/v2.2/films")
-    const data = await baseFetcher.getData();
-    const films = data.slice(0,9);
-    console.log(films)
-    LiveShowImages = films.map((movie) => {
-        const genresArray = movie.genres.map((item)=>item.genre);
-        const countriesArray = movie.countries.map((item) => item.country); 
-        return {
-            id: movie.kinopoiskId || movie.imdbId,
-            name:  movie.nameRu || movie.nameOriginal,
-            imageUrl: movie.posterUrlPreview,
-            rating: `Rating from the Kinopoisk: ${movie.ratingKinopoisk}`,
-            genre: `Genres: ${genresArray.join(", ")}`,
-            year: `Year: ${movie.year}`,
-            countries: `Countries: ${countriesArray.join(', ')}`,
-        }
-    })
-}
-
-await getMovies();
+import Arrow from '@/assets/images/icons/arrow.svg'
+import TubeSpinner from '@/assets/images/icons/tube-spinner.svg'
+import { MovieService } from '@/composables/services/MovieService.js';
 
 export default function NewLiveShow() {
+    const [moviesData, setMoviesData] = useState([]);
+
+    useLayoutEffect(() => {
+        const { getMovies } = MovieService();
+        const fetchMovies = async () => {
+            const data = await getMovies();
+            setMoviesData(data);
+        }
+        fetchMovies();
+    }, []);
+
     return (
         <section className="liveshow">
             <div className="liveshow__header">
                 <div className="liveshow__title">
-                    <div><span className="liveshow__title-text">Live Show</span></div>
+                    <div><span className="liveshow__title-text">Movies</span></div>
                     <div className="liveshow__more-options flex-container">
                         <span className="full-assortment">See all</span>
-                        <img src={Arrow} alt="arrow" />
+                        <img src={Arrow} alt="arrow" className="liveshow__arrow-icon" />
                     </div>
                 </div>
             </div>
             <div className="liveshow__assortment-list flex-centered">
-                {LiveShowImages.map(item => (
-                    <div className="liveshow__assortment-card" key={item.id}>
-                        <div className="assortment-card__icon">
-                            <img src={item.imageUrl} alt={item.name} />
+                {moviesData && moviesData.length > 0 ? (
+                    moviesData.map(item => (
+                        <div className="liveshow__assortment-card" key={item.id}>
+                            <div className="assortment-card__icon">
+                                <img src={item.imageUrl} alt={item.name} />
+                            </div>
+                            <div className="assortment-card__info">
+                                <div><span className="assortment-card__info-title">{item.name}</span></div>
+                                <div className="assortment-card__info-watches">{item.rating}</div>
+                                <div className="assortment-card__info-duration">{item.genre}</div>
+                                <div className="assortment-card__info-year">{item.year}</div>
+                                <div className="assortment-card__info-countries">{item.countries}</div>
+                            </div>
                         </div>
-                        <div className="assortment-card__info">
-                            <div><span className="assortment-card__info-title">{item.name}</span></div>
-                            <div className="assortment-card__info-watches">{item.rating}</div>
-                            <div className="assortment-card__info-duration">{item.genre}</div>
-                            <div className="assortment-card__info-year">{item.year}</div>
-                            <div className="assortment-card__info-countries">{item.countries}</div>
-                        </div>
+                    ))
+                ) : (
+                    <div>
+                       <img src={TubeSpinner} alt='tube-spinner' />
                     </div>
-                ))}
+                )}
             </div>
         </section>
     );
